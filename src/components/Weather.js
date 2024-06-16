4f74417ddac44b6b990318d06a59cf3eea9bfb0f
Shivaji Bodhas <131278682+Shivaji-Bodhas@users.noreply.github.com>
2024-06-16 10:53:36 +0530

    Update Weather.js

--- src/components/Weather.js
+++ src/components/Weather.js
@@ -4,11 +4,13 @@ const Weather = ({ weatherData }) => {
   if (!weatherData) return <div>No Data</div>;
 
   const { name, main, weather, wind } = weatherData;
+  const currentDate = new Date().toLocaleString();
 
   return (
     <div className="weather-info">
       <h2>{name}</h2>
       <p>Temperature: {main.temp} °C</p>
+      <p>Date and Time: {currentDate}</p>
       <p>Humidity: {main.humidity} %</p>
       <p>Wind Speed: {wind.speed} m/s</p>
       <p>Description: {weather[0].description}</p>
@@ -17,3 +19,5 @@ const Weather = ({ weatherData }) => {
 };
 
 export default Weather;
+
+
